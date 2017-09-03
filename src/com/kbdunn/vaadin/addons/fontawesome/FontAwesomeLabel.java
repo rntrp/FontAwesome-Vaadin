eadea23046ed1e164fc99a5481bfc0c90693e914
Roman Trapickin <rtrapickin@gmail.com>
2017-09-03 16:32:17 +0200

    Fixed indentation, Unix line endings

--- src/com/kbdunn/vaadin/addons/fontawesome/FontAwesomeLabel.java
+++ src/com/kbdunn/vaadin/addons/fontawesome/FontAwesomeLabel.java
@@ -5,154 +5,171 @@ import com.vaadin.ui.Label;
 
 public class FontAwesomeLabel extends Label {
 
-	private static final long serialVersionUID = -1987552834791565983L;
-	
-	private String size, fixedWidth, border, spin, pull, rotate, flip, inverse, stackOrder, custom;
+    private static final long serialVersionUID = -1987552834791565983L;
+
+    private String size, fixedWidth, border, spin, pull, rotate, flip, inverse, stackOrder, custom;
     private FontAwesomeLabel stacked;
     private FontAwesome icon;
-    
+
     public FontAwesomeLabel(FontAwesome icon) {
-    	this.icon = icon;
-    	setContentMode(ContentMode.HTML);
-    	addAttachListener(new AttachListener() {
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			public void attach(AttachEvent event) {
-				applyChanges();
-			}
-    	});
+        this.icon = icon;
+        setContentMode(ContentMode.HTML);
+        addAttachListener(new AttachListener() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void attach(AttachEvent event) {
+                applyChanges();
+            }
+        });
     }
-    
+
     public void applyChanges() {
-    	super.setValue(getCssHtml());
+        super.setValue(getCssHtml());
     }
-    
+
     public String getCssClasses(boolean ignoreSize) {
-    	String classes = "fa " + icon.clazz;
-    	classes += !ignoreSize && size != null ? " " + size : "";
-    	classes += fixedWidth != null ? " " + fixedWidth : "";
-    	classes += border != null ? " " + border : "";
-    	classes += spin != null ? " " + spin : "";
-    	classes += pull != null ? " " + pull : "";
-    	classes += rotate != null ? " " + rotate : "";
-    	classes += flip != null ? " " + flip : "";
-    	classes += inverse != null ? " " + inverse : "";
-    	classes += stackOrder != null ? " " + stackOrder : "";
-    	return classes;
+        String classes = "fa " + icon.clazz;
+        classes += !ignoreSize && size != null ? " " + size : "";
+        classes += fixedWidth != null ? " " + fixedWidth : "";
+        classes += border != null ? " " + border : "";
+        classes += spin != null ? " " + spin : "";
+        classes += pull != null ? " " + pull : "";
+        classes += rotate != null ? " " + rotate : "";
+        classes += flip != null ? " " + flip : "";
+        classes += inverse != null ? " " + inverse : "";
+        classes += stackOrder != null ? " " + stackOrder : "";
+        return classes;
     }
-    
+
     public String getCssHtml() {
-    	if (stacked == null) {
-    		return "<i class=\"" + getCssClasses(false) + "\"" 
-    				+ (custom != null ? "style=\"" + custom + "\"" : "") + "></i>";
-    	} 
-    	// Get stack size (bottom object takes precedence). Clear individual sizes.
-    	String stackSize = size != null ? size : stacked.size != null ? stacked.size : null;
-    	return "<span class=\"fa-stack " + (stackSize != null ? stackSize : "") + "\">"
-    				+ "<i class=\"" + getCssClasses(true) + "\""
-    					+ (custom != null ? "style=\"" + custom + "\"" : "") + "></i>"
-    				+ "<i class=\"" + stacked.getCssClasses(true) + "\""
-        				+ (stacked.custom != null ? "style=\"" + stacked.custom + "\"" : "") + "></i>"
-    			+ "</span>";
+        if (stacked == null) {
+            return "<i class=\"" + getCssClasses(false) + "\"" + (custom != null ? "style=\"" + custom + "\"" : "")
+                    + "></i>";
+        }
+        // Get stack size (bottom object takes precedence). Clear individual sizes.
+        String stackSize = size != null ? size : stacked.size != null ? stacked.size : null;
+        return "<span class=\"fa-stack " + (stackSize != null ? stackSize : "") + "\">" + "<i class=\""
+                + getCssClasses(true) + "\"" + (custom != null ? "style=\"" + custom + "\"" : "") + "></i>"
+                + "<i class=\"" + stacked.getCssClasses(true) + "\""
+                + (stacked.custom != null ? "style=\"" + stacked.custom + "\"" : "") + "></i>" + "</span>";
     }
-    
+
     public FontAwesomeLabel setCustomStyle(String css) {
-    	custom = css; return this;
+        custom = css;
+        return this;
     }
-    
+
     public FontAwesomeLabel setSizeLg() {
-    	size = "fa-lg"; return this;
+        size = "fa-lg";
+        return this;
     }
-    
+
     public FontAwesomeLabel setSize2x() {
-    	size = "fa-2x"; return this;
+        size = "fa-2x";
+        return this;
     }
-    
+
     public FontAwesomeLabel setSize3x() {
-    	size = "fa-3x"; return this;
+        size = "fa-3x";
+        return this;
     }
-    
+
     public FontAwesomeLabel setSize4x() {
-    	size = "fa-4x"; return this;
+        size = "fa-4x";
+        return this;
     }
-    
+
     public FontAwesomeLabel setSize5x() {
-    	size = "fa-5x"; return this;
+        size = "fa-5x";
+        return this;
     }
-    
+
     public FontAwesomeLabel setSize6x() {
-    	size = "fa-6x"; return this;
+        size = "fa-6x";
+        return this;
     }
-    
+
     public FontAwesomeLabel fixedWidth() {
-    	fixedWidth = "fa-fw"; return this;
+        fixedWidth = "fa-fw";
+        return this;
     }
-    
+
     public FontAwesomeLabel enableBorder() {
-    	border = "fa-border"; return this;
+        border = "fa-border";
+        return this;
     }
-    
+
     public FontAwesomeLabel pullLeft() {
-    	pull = "fa-pull-left"; return this;
+        pull = "fa-pull-left";
+        return this;
     }
-    
+
     public FontAwesomeLabel pullRight() {
-    	pull = "fa-pull-right"; return this;
+        pull = "fa-pull-right";
+        return this;
     }
-    
+
     public FontAwesomeLabel spin() {
-    	spin = "fa-spin"; return this;
+        spin = "fa-spin";
+        return this;
     }
-    
+
     public FontAwesomeLabel rotate90() {
-    	rotate = "fa-rotate-90"; return this;
+        rotate = "fa-rotate-90";
+        return this;
     }
-    
+
     public FontAwesomeLabel rotate180() {
-    	rotate = "fa-rotate-180"; return this;
+        rotate = "fa-rotate-180";
+        return this;
     }
-    
+
     public FontAwesomeLabel rotate270() {
-    	rotate = "fa-rotate-270"; return this;
+        rotate = "fa-rotate-270";
+        return this;
     }
-    
+
     public FontAwesomeLabel flipHorizontal() {
-    	flip = "fa-flip-horizontal"; return this;
+        flip = "fa-flip-horizontal";
+        return this;
     }
-    
+
     public FontAwesomeLabel flipVertical() {
-    	flip = "fa-flip-vertical"; return this;
+        flip = "fa-flip-vertical";
+        return this;
     }
-    
+
     public FontAwesomeLabel inverseColor() {
-    	inverse = "fa-inverse"; return this;
+        inverse = "fa-inverse";
+        return this;
     }
-    
+
     public FontAwesomeLabel stack(FontAwesome overlay) {
-    	return stack(overlay.getLabel());
+        return stack(overlay.getLabel());
     }
-    
+
     public FontAwesomeLabel stack(FontAwesomeLabel overlay) {
-    	stacked = overlay;
-    	stackOrder = "fa-stack-1x";
-    	stacked.stackOrder = "fa-stack-2x";
-    	return this;
+        stacked = overlay;
+        stackOrder = "fa-stack-1x";
+        stacked.stackOrder = "fa-stack-2x";
+        return this;
     }
-    
+
     public FontAwesomeLabel reverseStackSize() {
-    	if (stackOrder != null && stacked != null) {
-	    	stackOrder = "fa-stack-2x"; 
-	    	stacked.stackOrder = "fa-stack-1x";
-	    	return this;
-    	}
-    	throw new UnsupportedOperationException("Cannot call reverseStackSize() before a stacked icon has been set.");
+        if (stackOrder != null && stacked != null) {
+            stackOrder = "fa-stack-2x";
+            stacked.stackOrder = "fa-stack-1x";
+            return this;
+        }
+        throw new UnsupportedOperationException("Cannot call reverseStackSize() before a stacked icon has been set.");
     }
-    
+
     public FontAwesomeLabel clearStack() {
-    	if (stacked != null) stacked.stackOrder = null;
-    	stacked = null;
-    	stackOrder = null;
-    	return this;
+        if (stacked != null)
+            stacked.stackOrder = null;
+        stacked = null;
+        stackOrder = null;
+        return this;
     }
 }
--- src/com/kbdunn/vaadin/addons/fontawesome/util/GenerateEnum.java
+++ src/com/kbdunn/vaadin/addons/fontawesome/util/GenerateEnum.java
@@ -16,98 +16,99 @@ import org.jsoup.nodes.Element;
 
 public class GenerateEnum {
 
-	private static final String SOURCE = "http://fortawesome.github.io/Font-Awesome/cheatsheet/";
-	private static final String ENUM_FILE = "src/com/kbdunn/vaadin/addons/fontawesome/FontAwesome.java";
-	private static final String ENUM_ENTRY_REGEX = "\\t[A-Z0-9_]+\\(\\\"fa-.+\\),";
-	
-	public static void main(String[] args) {
-			writeFile(getIcons());
-			System.out.println("Done.");
-	}
-	
-	private static Map<String, String> getIcons() {
-		Map<String, String> icons = new LinkedHashMap<String, String>();
-		try {
-			Document html = Jsoup.connect(SOURCE).get();
-			String desc = html.getElementsByClass("jumbotron").get(0).getElementsByTag("p").get(0).ownText();
-			System.out.println("Scraping icon data from '" + desc + "' [" + SOURCE + "]....");
-			
-			// Loop through icons, get class and hex, add to map
-			String cssClass, hex;
-			for (Element e : html.getElementsByClass("row").get(1).children()) { // Cheatsheet has only 1 .row element
-				cssClass = e.ownText();
-				hex = e.getElementsByClass("text-muted").last().ownText().replace("[&#x", "").replace(";]", "");
-				icons.put(cssClass, hex);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("\tRead " + icons.size() + " icons.");
-		return icons;
-	}
-	
-	private static void writeFile(Map<String, String> icons) {
-		System.out.println("Updating " + ENUM_FILE + "....");
-		File infile = new File(ENUM_FILE);
-		File outfile = new File(ENUM_FILE + ".tmp");
-		BufferedReader reader = null;
-		BufferedWriter writer = null;
-		try {
-			reader = new BufferedReader(new FileReader(infile));
-			writer = new BufferedWriter(new FileWriter(outfile));
-			String inline = null;
-			int current = 0;
-			int enumstart = 0;
-			while ((inline = reader.readLine()) != null) {
-				current++;
-				if (enumstart > 0) {
-					// Check if we're still in the enum
-					if (!inline.matches(ENUM_ENTRY_REGEX)) {
-						System.out.println("\tIcons end on line " + current + ". "
-								+ "Replaced " + (current - enumstart + 1) + " icons.");
-						enumstart = 0;
-					}
-				} else if (inline.contains("public enum FontAwesome")) {
-					// Enum entries start on next line
-					enumstart = current + 1;
-					System.out.println("\tIcons start on line " + enumstart + ".");
-					writer.write(inline + "\n");
-					
-					// Write the new enum entries
-					writeEnumLines(writer, icons);
-				} else {
-					// Copy the rest of the file
-					writer.write(inline + "\n");
-				}
-			}
-			
-			writer.close();
-			reader.close();
-			
-			// Delete FontAwesome.java
-			if (!infile.delete()) 
-				throw new IOException("Could not delete " + infile);
-			
-			// Rename .tmp file
-			if (!outfile.renameTo(infile)) 
-				throw new IOException("Could not rename " + outfile + " to " + infile);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static void writeEnumLines(BufferedWriter writer, Map<String, String> icons) throws IOException {
-		String outline = null;
-		int outlines = 0;
-		int count = icons.size();
-		for (Entry<String, String> icon : icons.entrySet()) {
-			outline = "\t" + icon.getKey().toUpperCase().replace("FA-",  "").replace("-", "_").replace("500PX", "_500PX");
-			outline += "(\"" + icon.getKey() + "\", ";
-			outline += "0x" + icon.getValue();
-			outline += ")" + (++outlines == count ? ";" : ",") + "\n";
-			
-			writer.write(outline);
-		}
-	}
+    private static final String SOURCE = "http://fortawesome.github.io/Font-Awesome/cheatsheet/";
+    private static final String ENUM_FILE = "src/com/kbdunn/vaadin/addons/fontawesome/FontAwesome.java";
+    private static final String ENUM_ENTRY_REGEX = "\\t[A-Z0-9_]+\\(\\\"fa-.+\\),";
+
+    public static void main(String[] args) {
+        writeFile(getIcons());
+        System.out.println("Done.");
+    }
+
+    private static Map<String, String> getIcons() {
+        Map<String, String> icons = new LinkedHashMap<String, String>();
+        try {
+            Document html = Jsoup.connect(SOURCE).get();
+            String desc = html.getElementsByClass("jumbotron").get(0).getElementsByTag("p").get(0).ownText();
+            System.out.println("Scraping icon data from '" + desc + "' [" + SOURCE + "]....");
+
+            // Loop through icons, get class and hex, add to map
+            String cssClass, hex;
+            for (Element e : html.getElementsByClass("row").get(1).children()) { // Cheatsheet has only 1 .row element
+                cssClass = e.ownText();
+                hex = e.getElementsByClass("text-muted").last().ownText().replace("[&#x", "").replace(";]", "");
+                icons.put(cssClass, hex);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\tRead " + icons.size() + " icons.");
+        return icons;
+    }
+
+    private static void writeFile(Map<String, String> icons) {
+        System.out.println("Updating " + ENUM_FILE + "....");
+        File infile = new File(ENUM_FILE);
+        File outfile = new File(ENUM_FILE + ".tmp");
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        try {
+            reader = new BufferedReader(new FileReader(infile));
+            writer = new BufferedWriter(new FileWriter(outfile));
+            String inline = null;
+            int current = 0;
+            int enumstart = 0;
+            while ((inline = reader.readLine()) != null) {
+                current++;
+                if (enumstart > 0) {
+                    // Check if we're still in the enum
+                    if (!inline.matches(ENUM_ENTRY_REGEX)) {
+                        System.out.println("\tIcons end on line " + current + ". " + "Replaced "
+                                + (current - enumstart + 1) + " icons.");
+                        enumstart = 0;
+                    }
+                } else if (inline.contains("public enum FontAwesome")) {
+                    // Enum entries start on next line
+                    enumstart = current + 1;
+                    System.out.println("\tIcons start on line " + enumstart + ".");
+                    writer.write(inline + "\n");
+
+                    // Write the new enum entries
+                    writeEnumLines(writer, icons);
+                } else {
+                    // Copy the rest of the file
+                    writer.write(inline + "\n");
+                }
+            }
+
+            writer.close();
+            reader.close();
+
+            // Delete FontAwesome.java
+            if (!infile.delete())
+                throw new IOException("Could not delete " + infile);
+
+            // Rename .tmp file
+            if (!outfile.renameTo(infile))
+                throw new IOException("Could not rename " + outfile + " to " + infile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeEnumLines(BufferedWriter writer, Map<String, String> icons) throws IOException {
+        String outline = null;
+        int outlines = 0;
+        int count = icons.size();
+        for (Entry<String, String> icon : icons.entrySet()) {
+            outline = "\t"
+                    + icon.getKey().toUpperCase().replace("FA-", "").replace("-", "_").replace("500PX", "_500PX");
+            outline += "(\"" + icon.getKey() + "\", ";
+            outline += "0x" + icon.getValue();
+            outline += ")" + (++outlines == count ? ";" : ",") + "\n";
+
+            writer.write(outline);
+        }
+    }
 }
